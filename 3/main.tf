7642c83e2f59775a4d0f53068e8be4cbe5294f85
Sawyer086 <145470451+Sawyer086@users.noreply.github.com>
2025-03-23 19:24:15 +0500

    Update main.tf

--- 3/main.tf
+++ 3/main.tf
@@ -45,52 +45,32 @@ resource "yandex_compute_instance" "platform" {
 
 }
 
-
 ### 3 zadanie
-
-resource "yandex_vpc_network" "develop2" {
-  name = var.vm_db_name
-}
-resource "yandex_vpc_subnet" "develop3" {
-  name           = var.vm_db_name
-  zone           = var.vm_db_default_zone
-  network_id     = yandex_vpc_network.develop2.id
-  v4_cidr_blocks = var.vm_db_default_cidr
-}
-
-
-data "yandex_compute_image" "ubuntu2" {
-  family = var.vm_web_family
-}
-
-resource "yandex_compute_instance" "netology-develop" {
-  name        = "netology-develop-platform-web2"
-  platform_id = "standard-v3"
-  zone        = var.vm_db_default_zone
-
+resource "yandex_compute_instance" "platform_db" {
+  name        = var.vm_db_name
+  zone        = var.vm_db_zone
+  platform_id = var.vm_db_platform_id
   resources {
-    core_fraction = 20
-    cores         = 2
-    memory        = 2
+    cores         = var.vm_db_cores
+    memory        = var.vm_db_memory
+    core_fraction = var.vm_db_core_fraction
   }
-
   boot_disk {
     initialize_params {
-      image_id = data.yandex_compute_image.ubuntu2.image_id
+      image_id = data.yandex_compute_image.ubuntu.image_id
     }
   }
   scheduling_policy {
-    preemptible = true
-  }
+    preemptible = var.vm_db_preemptible
+  } 
   network_interface {
-    subnet_id = yandex_vpc_subnet.develop3.id
-    nat       = true
+    subnet_id = yandex_vpc_subnet.develop.id
+    nat       = var.vm_db_nat
   }
 
-#  /*metadata = {
-#    serial-port-enable = 1
-#    ssh-keys           = "ubuntu:${var.vm_db_ssh_root_key}"
-#  }*/
+  metadata = {
+    serial-port-enable = var.vm_db_serial_port_enable
+    ssh-keys           = "ubuntu:${var.vms_ssh_root_key}"
+  }
 
-  metadata = var.vms_ssh_root_key
-
+}
