1acd4e82b4e335b2d8f833481ea1b15b3f3adaa8
Sawyer086 <145470451+Sawyer086@users.noreply.github.com>
2025-03-24 17:01:10 +0500

    Update main.tf

--- 2/main.tf
+++ 2/main.tf
@@ -1,45 +1,43 @@
 resource "yandex_vpc_network" "develop" {
-  name = var.vpc_name
+  name = var.vpc_name
 }
 resource "yandex_vpc_subnet" "develop" {
-  name           = var.vpc_name
-  zone           = var.default_zone
-  network_id     = yandex_vpc_network.develop.id
-  v4_cidr_blocks = var.default_cidr
+  name           = var.vpc_name
+  zone           = var.default_zone
+  network_id     = yandex_vpc_network.develop.id
+  v4_cidr_blocks = var.default_cidr
 }
 
 
 data "yandex_compute_image" "ubuntu" {
-#  family = "ubuntu-2004-lts"
-# 2 zadanie
-  family = var.vm_web_family
+  family = var.vm_web_family
 }
 resource "yandex_compute_instance" "platform" {
-  name        = "netology-develop-platform-web"
-  platform_id = "standard-v3"
-  zone        = var.default_zone
+  name        = var.vm_web_name
+  platform_id = var.vm_web_platform_id
+  zone        = var.default_zone
 
-  resources {
-    cores         = 2
-    memory        = 1
-    core_fraction = 20
-  }
-  boot_disk {
-    initialize_params {
-      image_id = data.yandex_compute_image.ubuntu.image_id
-    }
-  }
-  scheduling_policy {
-    preemptible = true
-  }
-  network_interface {
-    subnet_id = yandex_vpc_subnet.develop.id
-    nat       = true
-  }
+  resources {
+    cores         = var.vm_web_cores
+    memory        = var.vm_web_memory
+    core_fraction = var.vm_web_core_fraction
+  }
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.image_id
+    }
+  }
+  scheduling_policy {
+    preemptible = true
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop.id
+    nat       = true
+  }
 
-  metadata = {
-    serial-port-enable = 1
-    ssh-keys           = "ubuntu:${var.vms_ssh_root_key}"
-  }
+  metadata = {
+    serial-port-enable = 1
+    ssh-keys           = "ubuntu:${var.vms_ssh_root_key}"
+  }
 
 }
