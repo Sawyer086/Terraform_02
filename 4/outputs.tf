bc2f286b7966d2c16e42b611eef25634f6141a84
Sawyer086 <145470451+Sawyer086@users.noreply.github.com>
2025-03-27 21:08:14 +0500

    Update outputs.tf

--- 4/outputs.tf
+++ 4/outputs.tf
@@ -1,1 +1,8 @@
-1
+output "vm_details" {
+  description = "Details of the VM: instance_name, external_ip, fqdn"
+  value = {
+    instance_name = yandex_compute_instance.platform.name
+    external_ip   = yandex_compute_instance.platform.network_interface[0].nat_ip
+    fqdn          = "${yandex_compute_instance.platform.name}.example.com"  # Можно заменить на свой домен
+  }
+}
