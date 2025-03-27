854800a65bb380de8b71d497ee118ece845c9ada
Sawyer086 <145470451+Sawyer086@users.noreply.github.com>
2025-03-27 21:12:34 +0500

    Update outputs.tf

--- 4/outputs.tf
+++ 4/outputs.tf
@@ -1,8 +1,15 @@
-output "vm_details" {
-  description = "Details of the VM: instance_name, external_ip, fqdn"
+output "vm_instances" {
   value = {
-    instance_name = yandex_compute_instance.platform.name
-    external_ip   = yandex_compute_instance.platform.network_interface[0].nat_ip
-    fqdn          = "${yandex_compute_instance.platform.name}.example.com"  # Можно заменить на свой домен
+    platform = {
+      instance_name = yandex_compute_instance.platform.name
+      external_ip   = yandex_compute_instance.platform.network_interface[0].nat_ip_address
+      fqdn          = "${yandex_compute_instance.platform.name}.auto.internal"
+    }
+    platform_db = {
+      instance_name = yandex_compute_instance.platform_db.name
+      external_ip   = yandex_compute_instance.platform_db.network_interface[0].nat_ip_address
+      fqdn          = "${yandex_compute_instance.platform_db.name}.auto.internal"
+    }
   }
+  description = "Информация о виртуальных машинах"
 }
