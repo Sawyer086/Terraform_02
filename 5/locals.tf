ed15df9dd87b8a3b883e24ea7bf983e65d393afa
Sawyer086 <145470451+Sawyer086@users.noreply.github.com>
2025-03-27 21:47:11 +0500

    Update locals.tf

--- 5/locals.tf
+++ 5/locals.tf
@@ -1,4 +1,7 @@
 locals {
-  vm_web_name = "netology-${var.vm_web_env}-${var.vm_web_project}-${var.vm_web_role}"
-  vm_db_name = "netology-${var.vm_db_env}-${var.vm_db_project}-${var.vm_db_role}"
+  lplatform    = "netology-develop-platform"
+  lweb         = "web"
+  ldb          = "db"
+  vm_web_lname = "${local.lplatform}-${local.lweb}"
+  vm_db_lname  = "${local.lplatform}-${local.ldb}"
 }
