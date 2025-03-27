7b5061b6dd0243d788a51ec9cafdf474e3ce8444
Sawyer086 <145470451+Sawyer086@users.noreply.github.com>
2025-03-27 20:29:08 +0500

    Update main.tf

--- 3/main.tf
+++ 3/main.tf
@@ -1,81 +1,4 @@
-resource "yandex_vpc_network" "develop" {
-  name = var.vpc_name
-}
-resource "yandex_vpc_subnet" "develop" {
-  name           = var.vpc_name
-  zone           = var.default_zone
-  network_id     = yandex_vpc_network.develop.id
-  v4_cidr_blocks = var.default_cidr
-}
-
-
-data "yandex_compute_image" "ubuntu" {
-  family = var.vm_web_family
-}
-resource "yandex_compute_instance" "platform" {
-  name        = var.vm_web_name
-  platform_id = var.vm_web_platform_id
-  zone        = var.default_zone
-
-  resources {
-    cores         = var.vm_web_cores
-    memory        = var.vm_web_memory
-    core_fraction = var.vm_web_core_fraction
-  }
-  boot_disk {
-    initialize_params {
-      image_id = data.yandex_compute_image.ubuntu.image_id
-    }
-  }
-  scheduling_policy {
-    preemptible = true
-  }
-  network_interface {
-    subnet_id = yandex_vpc_subnet.develop.id
-    nat       = true
-  }
-
-  metadata = {
-    serial-port-enable = 1
-    ssh-keys           = "ubuntu:${var.vms_ssh_root_key}"
-  }
-
-}
-
-resource "yandex_compute_instance" "platform_db" {
-  name        = var.vm_db_name
-  zone        = var.vm_db_zone
-  platform_id = var.vm_db_platform_id
-  resources {
-    cores         = var.vm_db_cores
-memory        = var.vm_db_memory
-    core_fraction = var.vm_db_core_fraction
-  }
-  boot_disk {
-    initialize_params {
-      image_id = data.yandex_compute_image.ubuntu.image_id
-    }
-  }
-  scheduling_policy {
-    preemptible = var.vm_db_preemptible
-  }
-  network_interface {
-    subnet_id = yandex_vpc_subnet.develop.id
-    nat       = var.vm_db_nat
-  }
-
-  metadata = {
-    serial-port-enable = var.vm_db_serial_port_enable
-    ssh-keys           = "ubuntu:${var.vms_ssh_root_key}"
-  }
-
-}
-
-
-
-
-###
-resource "yandex_vpc_network" "develop" {
+esource "yandex_vpc_network" "develop" {
   name = var.vpc_name
 }
 
@@ -94,7 +17,7 @@ data "yandex_compute_image" "ubuntu" {
 resource "yandex_compute_instance" "platform" {
   name        = var.vm_web_name
   platform_id = var.vm_web_platform_id
-  zone        = var.vm_web_zone  # Зона для первой ВМ
+  zone        = var.default_zone  # Зона для первой ВМ
 
   resources {
     cores         = var.vm_web_cores
@@ -124,6 +47,13 @@ resource "yandex_compute_instance" "platform" {
 }
 
 # Вторая ВМ, созданная в зоне ru-central1-b
+resource "yandex_vpc_subnet" "develop_b" {
+  name           = "${var.vpc_name}-b"
+  zone           = "ru-central1-b"
+  network_id     = yandex_vpc_network.develop.id
+  v4_cidr_blocks = var.second_cidr  # Добавьте новый диапазон IP
+}
+
 resource "yandex_compute_instance" "platform_db" {
   name        = var.vm_db_name
   zone        = "ru-central1-b"  # Прямо указываем зону для второй ВМ
@@ -146,7 +76,7 @@ resource "yandex_compute_instance" "platform_db" {
   }
 
   network_interface {
-    subnet_id = yandex_vpc_subnet.develop.id
+    subnet_id = yandex_vpc_subnet.develop_b.id
     nat       = var.vm_db_nat
   }
 
